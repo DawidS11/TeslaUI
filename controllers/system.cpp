630ccdc018eafd64cf3b7f409f76c51ef0bbca30
DawidS11 <dawidsmalcuga@gmail.com>
2024-05-21 17:10:52 +0200

    Created system files

---
+++ controllers/system.cpp
@@ -0,0 +1,5 @@
+#include "system.h"
+
+System::System(QObject *parent)
+    : QObject{parent}
+{}
---
+++ controllers/system.h
@@ -0,0 +1,15 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <QObject>
+
+class System : public QObject
+{
+    Q_OBJECT
+public:
+    explicit System(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // SYSTEM_H
