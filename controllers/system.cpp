baa3584bd0c8e6338e8d6eae0f669c25c4c94202
DawidS11 <dawidsmalcuga@gmail.com>
2024-05-22 21:48:29 +0200

    Added Q_Properties to system

--- controllers/system.cpp
+++ controllers/system.cpp
@@ -2,4 +2,38 @@
 
 System::System(QObject *parent)
     : QObject{parent}
+    , m_carLocked(true)
+    , m_outdoorTemp(21)
+    ,m_userName("username")
 {}
+
+bool System::name() const
+{
+    return m_carLocked;
+}
+
+int System::outdoorTemp() const
+{
+    return m_outdoorTemp;
+}
+
+void System::setOutdoorTemp(int newOutdoorTemp)
+{
+    if (m_outdoorTemp == newOutdoorTemp)
+        return;
+    m_outdoorTemp = newOutdoorTemp;
+    emit outdoorTempChanged();
+}
+
+QString System::userName() const
+{
+    return m_userName;
+}
+
+void System::setUserName(const QString &newUserName)
+{
+    if (m_userName == newUserName)
+        return;
+    m_userName = newUserName;
+    emit userNameChanged();
+}
--- controllers/system.h
+++ controllers/system.h
@@ -6,10 +6,30 @@
 class System : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(bool carLocked READ name carLocked setname setCarLocked nameChanged carLockedChanged)
+    Q_PROPERTY(int outdoorTemp READ outdoorTemp WRITE setOutdoorTemp NOTIFY outdoorTempChanged FINAL)
+    Q_PROPERTY(QString userName READ userName WRITE setUserName NOTIFY userNameChanged FINAL)
+
 public:
     explicit System(QObject *parent = nullptr);
 
+    bool name() const;
+
+    int outdoorTemp() const;
+    void setOutdoorTemp(int newOutdoorTemp);
+
+    QString userName() const;
+    void setUserName(const QString &newUserName);
+
 signals:
+    void outdoorTempChanged();
+
+    void userNameChanged();
+
+private:
+    bool m_carLocked;
+    int m_outdoorTemp;
+    QString m_userName;
 };
 
 #endif // SYSTEM_H
